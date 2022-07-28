b1db99ed9d1ec359022abae1c8901731b70503a1
Lucaponz91 <lucapo1991@msn.com>
2022-07-28 15:36:24 +0200

    pari dispari add function

--- Pari e Dispari/js/app.js
+++ Pari e Dispari/js/app.js
@@ -10,3 +10,11 @@ const numeroUser = parseInt(prompt('Scegli un numero da 1 a 5'));
 const numeroIA = Math.floor(Math.random() * 5 ) + 1;
 const somma = numeroUser + numeroIA
 console.log( numeroUser, numeroIA, somma);
+
+function checkPariDispari(somma) {
+	if (somma%2 == 0)
+		return true;
+	else
+		return false;
+}
+console.log(checkPariDispari(somma))
